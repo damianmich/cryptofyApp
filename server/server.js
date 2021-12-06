7c814ea500c9d3003655424c931953d38ed10921
damianmich <damian.mich1993@gmail.com>
2021-12-06 21:41:54 +0100

    Setup production build v2

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
